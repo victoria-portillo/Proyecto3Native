3e43b8827ac6f8777730bc0e750e62cadfeb78a2
victoria-portillo <vportillo@udesa.edu.ar>
2024-06-11 17:47:53 -0300

    cambios en agregar posteos

--- proyectoFinal/src/screens/PosteoNuevo.js
+++ proyectoFinal/src/screens/PosteoNuevo.js
@@ -4,142 +4,124 @@ import { db, auth } from '../firebase/config'
 import CamaraPosteo from '../components/CamaraPosteo';
 
 export default class PosteoNuevo extends Component {
-    constructor(props){
-      super(props)
-      this.state = {
+  constructor(props){
+    super(props)
+    this.state = {
+      descripcion:'',
+      urlFoto:'',
+      paso1:true
+    }
+  }
+
+  onSubmit(){
+    const { descripcion, urlFoto } = this.state;
+    db.collection('posteos').add({
+      owner: auth.currentUser.email,
+      createdAt: Date.now(),
+      fotoUrl: urlFoto,
+      descripcion: descripcion,
+      likes:[]
+    })
+    .then(() => {
+      this.setState({
         descripcion:'',
         urlFoto:'',
         paso1:true
-      }
-    }
-  
-    onSubmit({
-      descripcion,
-      fotoUrl
-    }){
-      db.collection('posteos').add(
-        {
-          owner: auth.currentUser.email,
-          createdAt:Date.now(),
-          fotoUrl:fotoUrl,
-          descripcion:descripcion,
-          likes:[]
-        }
-      )
-      .then(() => this.props.navigation.navigate('Home'))
-      .catch((e) => console.log(e))
-    }
-  
-    actualizarDescripcion(text){
-      this.setState({
-        descripcion: text
-      })
-    }
-  
-    actualizarFotourl(url){
-      this.setState({
-        urlFoto: url,
-        paso1: false
-      })
-    }
-  
-    render() {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.text}>Nuevo Posteo</Text>
-          {
-            this.state.paso1 ?
-              <View style={styles.cameraContainer}>
-                <CamaraPosteo
-                  actualizarFotourl={(url) => this.actualizarFotourl(url)}
-                />
-              </View>
-            :
-            <>
-              <TextInput
-                style={styles.descriptionInput}
-                placeholder="Descripción"
-                onChangeText={(descripcion) => this.actualizarDescripcion(descripcion)}
-                value={this.state.descripcion}
-              />
-              <TouchableOpacity
-                style={styles.submitButton}
-                onPress={() => this.onSubmit({
-                  descripcion: this.state.descripcion,
-                  fotoUrl: this.state.urlFoto
-                })}
-              >
-                <Text style={styles.submitButtonText}>Enviar</Text>
-              </TouchableOpacity>
-            </>
-          }
-        </View>
-      )
-    }
+      });
+      this.props.navigation.navigate('Home');
+    })
+    .catch((error) => console.error("Error al enviar el posteo:", error));
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 16,
-      backgroundColor: '#9fc1ad',
-    },
-    text: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 16,
-    },
-    button: {
-      backgroundColor: 'blue',
-      padding: 12,
-      borderRadius: 8,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-    input: {
-      width: '100%',
-      borderColor: 'gray',
-      borderWidth: 1,
-      borderRadius: 8,
-      padding: 8,
-      marginBottom: 16,
-    },
-    cameraContainer: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-  
-    },
-    imagePreview: {
-      width: 500,
-      height: 500,
-      marginBottom: 16,
-    },
-    descriptionInput: {
-      flex: 1,
-      width: '50%',
-      height: '50%',
-      borderColor: 'gray',
-      borderWidth: 1,
-      borderRadius: 8,
-      padding: 10,
-      marginBottom: 20,
-    },
-    submitButton: {
-      backgroundColor: '#5F866F',
-      padding: 12,
-      borderRadius: 8,
-      alignItems: 'center',
-    },
-    submitButtonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-  });
+
+  actualizarDescripcion(text){
+    this.setState({
+      descripcion: text
+    })
+  }
+
+  actualizarFotourl(url){
+    this.setState({
+      urlFoto: url,
+      paso1: false
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Nuevo Posteo</Text>
+        {
+          this.state.paso1 ?
+            <View style={styles.cameraContainer}>
+              <CamaraPosteo
+                actualizarFotourl={(url) => this.actualizarFotourl(url)}
+              />
+            </View>
+          :
+          <>
+            <TextInput
+              style={styles.descriptionInput}
+              placeholder="Descripción"
+              onChangeText={(descripcion) => this.actualizarDescripcion(descripcion)}
+              value={this.state.descripcion}
+            />
+            <TouchableOpacity
+              style={styles.submitButton}
+              onPress={() => this.onSubmit()}
+            >
+              <Text style={styles.submitButtonText}>Enviar</Text>
+            </TouchableOpacity>
+          </>
+        }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#9fc1ad',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 16,
+  },
+  cameraContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  descriptionInput: {
+    width: '50%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 20,
+  },
+  submitButton: {
+    backgroundColor: '#5F866F',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  submitButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
